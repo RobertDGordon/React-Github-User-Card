fcb0bd2618e6601ac9bb67b59d55208ed32dfaac
Robert Gordon <Komeuppance@gmail.com>
2019-11-26 23:31:45 -0800

    Search function working, rate limit exceeded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCard from './components/UserCard';
+import SearchFrom from './components/SearchForm';
 import './App.css';
 
 class App extends React.Component {
@@ -17,29 +18,70 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch(`https://api.github.com/users/${this.state.currentUser}`)
+    fetch(`https://api.github.com/rate_limit`)
     .then (res => res.json())
     .then (res => {
-      // console.log(res);
-      this.setState({gitHubData: res});
-      // console.log(this.state.gitHubData)
+      console.log(res);
     })
     .catch(err => {console.log(err)})
 
+    fetch(`https://api.github.com/users/${this.state.currentUser}`)
+      .then (res => res.json())
+      .then (res => {
+        // console.log(res);
+        this.setState({gitHubData: res});
+        // console.log(this.state.gitHubData)
+      })
+      .catch(err => {console.log(err)})
+
     fetch(`http://api.github.com/users/${this.state.currentUser}/followers`)
-    .then (res => res.json())
-    .then (res => {
-      res.forEach(item => {
-        fetch(item.url)
-        .then (res => res.json())
-        .then (res => {
-          // console.log (this.state.followers, 'follower forEach')
-          this.setState({followers: [...this.state.followers, res ]})
-        })
+      .then (res => res.json())
+      .then (res => {
+        res.forEach(item => {
+          fetch(item.url)
+          .then (res => res.json())
+          .then (res => {
+            // console.log (this.state.followers, 'follower forEach')
+            this.setState({followers: [...this.state.followers, res ]})
+          })
         .catch(err => {console.log(err, 'forEach error')})
       })
     })
     .catch(err => {console.log(err, 'follower error')})
+  }
+
+  searchUser = userName => {
+    this.setState({ currentUser: userName})
+    console.log (this.state.currentUser)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentUser !== this.state.currentUser) {
+      console.log ('component did update!')
+      fetch(`https://api.github.com/users/${this.state.currentUser}`)
+        .then (res => res.json())
+        .then (res => {
+          this.setState({gitHubData: res});
+          //Clear followers list, and then fetch new data
+          // this.setState({followers: []})
+          // fetch(`http://api.github.com/users/${this.state.currentUser}/followers`)
+          //   .then (res => res.json())
+          //   .then (res => {
+          //     res.forEach(item => {
+          //       fetch(item.url)
+          //       .then (res => res.json())
+          //       .then (res => {
+          //         // console.log (this.state.followers, 'follower forEach')
+          //         this.setState({followers: [...this.state.followers, res ]})
+          //       })
+          //     .catch(err => {console.log(err, 'forEach error')})
+          //   })
+          // })
+          // .catch(err => {console.log(err, 'follower error')})
+          //end fetching new followers
+        })
+        .catch(err => {console.log(err)})
+    }
   }
 
   render() {
@@ -47,6 +89,7 @@ class App extends React.Component {
       <div>
         <header>
           <h1>GitHub</h1>
+          <SearchFrom searchUser={this.searchUser} />
         </header>
         <UserCard
           {...this.state.gitHubData}
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 class SearchFrom extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            userName: ""
+        };
+    }
+    handleChanges = e => {
+        this.setState({
+            userName: e.target.value
+        });
+    };
 
+    handleSubmit = e => {
+        e.preventDefault();
+        if (this.state.userName !== '') {
+            this.props.searchUser(this.state.userName);
+            this.setState({
+                userName:''
+            });
+        }
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    onChange={this.handleChanges}
+                    type='text'
+                    name='User'
+                    value={this.state.userName}
+                />
+                <button>Git!</button>
+            </form>
+        )
+    }
 }
 
 export default SearchFrom
