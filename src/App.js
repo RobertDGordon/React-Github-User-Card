e8a999e67be1898e5bb4e2ae17c826b446892545
Robert Gordon <Komeuppance@gmail.com>
2019-11-26 21:12:55 -0800

    Data recieved successfully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 
 class App extends React.Component {
 
+  state = {
+    gitHubData:[]
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/RobertDGordon')
+    .then (res => res.json())
+    .then (res => {
+      console.log(res);
+      this.setState({gitHubData: res.message});
+    })
+    .catch(err => {console.log(err)})
+  }
+
   render() {
     return (
       <div className="App">
